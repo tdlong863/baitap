2a9cd22936ad8b6f76919b9df1b82dc170d41046
tdlong863 <tdlong86203@gmail.com>
2023-02-28 21:54:39 +0700

    bt2402

--- baithuchanh2402/src/baithuchanh2402/slide30.java
+++ baithuchanh2402/src/baithuchanh2402/slide30.java
@@ -3,6 +3,17 @@ package baithuchanh2402;
 public class slide30 {
 
 	public static void main(String[] args) {
+		  int n, remainder, sum = 0;
+	        Scanner sc = new Scanner(System.in);
+	        System.out.println("Nhap n:");
+	        n = sc.nextInt();
+
+	        while(n > 0) {
+	            remainder = n % 10;
+	            n /= 10;
+	            sum += remainder;
+	        }
+	        System.out.println("Tong cac chu so la: " + sum);
 		// TODO Auto-generated method stub
 
 	}
--- baithuchanh2402/src/baithuchanh2402/slide35.java
+++ baithuchanh2402/src/baithuchanh2402/slide35.java
@@ -1,10 +1,14 @@
-package baithuchanh2402;
+import java.util.Scanner;
 
-public class slide35 {
+public class Slide35 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+        System.out.print("Nhập số thứ nhất : ");
+        int num1 = scanner.nextInt();
+        System.out.print("Nhập số thứ hai: ");
+        int num2 = scanner.nextInt();
+        int minNum = num1 < num2 ? num1 : num2;
+        System.out.println("Số nhỏ nhất là: " + minNum);
+    }
 }
